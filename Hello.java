1926a42747849dc61dea95a1f3e098d3781521ef
Tanushr23cs <rtanushjeeva2005@gmail.com>
2026-07-13 18:45:42 +0530

    Webhook test

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins CI/CD");
         System.out.println("My Name is Tanu");
-        System.out.println("Learning Jenkins Integration");
+        System.out.println("This is jenkins webhook testing, yeah it was working!");
     }
 }
