2b6a9ff0927d636f1bb02388d5970235661d32c3
Tanushr23cs <rtanushjeeva2005@gmail.com>
2026-07-13 18:00:10 +0530

    Initial Java program

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins CI/CD");
+        System.out.println("My Name is Tanu");
+        System.out.println("Learning Jenkins Integration");
+    }
+}
